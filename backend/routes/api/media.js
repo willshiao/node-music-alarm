ac67cceb640855833200bcf98cc10f0b18e3bd25
William Shiao <willshiao@gmail.com>
2016-11-03 16:09:54 -0700

    feat(api): add the ability to batch add media

--- backend/routes/api/media.js
+++ backend/routes/api/media.js
@@ -12,8 +12,27 @@ router.get('/', (req, res) => {
 });
 
 router.post('/new', (req, res) => {
-  if(!req.body || !req.body.path || !req.body.name)
-    return res.failMsg('Missing one or more arguements');
+  if(!req.body)
+    return res.failMsg('No arguments found');
+  if(req.body.media && req.body.media.constructor === Array
+  && req.body.media.length > 0) {
+    //Treat as multiple elements
+    const media = req.body.media
+      .map(m => {
+        if(!m || !m.path || !m.name) return null;
+        return new Media(m).save();
+      })
+      .filter(m => m !== null);
+
+    if(media.length <= 0)
+      return res.failMsg('No valid elements found');
+    return Promise.all(media)
+      .then(() => {
+        res.successJson({numSaved: media.length});
+      })
+      .catch(err => res.errorJson(err));
+  } else if(!req.body.path || !req.body.name)
+    return res.failMsg('Missing one or more arguments');
 
   new Media({
     path: req.body.path,
