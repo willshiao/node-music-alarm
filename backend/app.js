c2cec5b96fddfa7306e5fcc2edd2a30c2ebee6f0
William Shiao <willshiao@gmail.com>
2017-06-20 17:48:24 -0700

    feat(api): Make backend use new model format

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const config = require('config');
-const Promise = require('bluebird');
-const fs = Promise.promisifyAll(require('fs'));
 const express = require('express');
 
 const Alarm = require('./models/Alarm');
+const Media = require('./models/Media');
 const db = require('./lib/db');
+const AlarmHelper = require('./lib/alarm-helper');
 const logger = require('./lib/logger');
 require('./lib/extend').extendResponse(express.response);
 
@@ -15,34 +15,19 @@ const apiRoutes = require('./routes/api');
 const app = express();
 app.use('/api', apiRoutes);
 
-let createTables = false;
 
-fs.statAsync(config.get('db.path'))
-  .catch((err) => {
-    if(err.code === 'ENOENT') {
-      logger.debug('DB not found, going to create tables.');
-      createTables = true;
-    } else {
-      logger.error('Error: ', err);
-    }
-  })
-  .then(() => db.open(config.get('db.path')))
-  .then(() => {
-    logger.debug('Connected to DB successfully.');
-    if(!createTables) return Promise.resolve();
-    logger.debug('Creating tables...');
-    return db.createTables();
-  })
+Alarm.sync({ force: true })
+  .then(() => Media.sync({ force: true }))
   .then(() => {
     const port = process.env.PORT || config.get('port');
     app.listen(port, () => {
       logger.info(`App listening on port ${port}.`);
     });
     logger.debug('Loading alarms from the database.');
-    return Alarm.getAll(true);  // Only get enabled alarms
+    return Alarm.findAll({ where: { enabled: true } });  // Only get enabled alarms
   })
-  .then(alarms => Promise.all(alarms.map(a => a.schedule())))
-  .then(() => {
+  .then((alarms) => {
+    alarms.forEach(alarm => AlarmHelper.schedule(alarm));
     logger.info('Done loading all alarms.');
   })
   .catch(logger.error);
